a66289578ff9d6b8e750b2f74bec0018cf6f402e
mahabub775 <mahabub775@gmail.com>
2024-05-27 12:32:43 +0600

    modify

--- BSPOS.Core/Model/ProductModel.cs
+++ BSPOS.Core/Model/ProductModel.cs
@@ -5,6 +5,7 @@ namespace BSPOS.Core.Model;
 public class ProductModel : AuditModel
 {
 	public int Id { get; set; }
+	public string Code { get; set; }
 
 	[Required(ErrorMessage = "Please enter 'Name'.")]
 	[MinLength(3, ErrorMessage = "Minimum length of 'Name' is 3 characters.")]
